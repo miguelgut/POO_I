9b07acbccb5f7b9abb5e035f32a590e1de436b5c
Miguel Gut Seara <sistema3@comunicareaparelhosauditivos.com>
2020-04-07 20:11:46 -0300

    Novos métodos

--- App.java
+++ App.java
@@ -1,9 +1,22 @@
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+
 class App{
 
     public static void main(String[] args) {
-        ContaCorrente miguel = new ContaCorrente();
-        miguel.deposita(100.00);
-        System.out.println(miguel.getSaldo());
+       
+        ArrayList<ContaCorrente> contas = new ArrayList<ContaCorrente>();
+        
+        contas.add(new ContaCorrente(200));
+        contas.add(new ContaCorrente());
+        contas.add(new ContaCorrente(1000, "marcos"));
+
+        System.out.println("\n\nLeitura de saldos: \n");
+    
+        contas.forEach((conta) ->  System.out.println("Conta : "+ conta.getSaldo() + " Titular: " + conta.getTitular()));
+
     }
     
 }
--- ContaCorrente.java
+++ ContaCorrente.java
@@ -1,13 +1,37 @@
 class ContaCorrente{
-
     private double saldo;
+    private String nome;
 
-    public void deposita(double valor){
+    //Método construtor, valido para inicializar uma conta já com um saldo
+    public ContaCorrente(double valor, String nome){
+        this.saldo = valor;
+        this.nome = nome;
+   }
+    
+    public ContaCorrente(double valor){
+         this.saldo = valor;
+         this.nome = "";
+    }
+    
+    public ContaCorrente(){
+        this.saldo = 0;
+        this.nome = "";
+   }
+
+    public void depositar(double valor){
         this.saldo = this.saldo + valor;
+    }
+
+    public void sacar(double valor){
+        this.saldo = this.saldo - valor;
     }
 
     public double getSaldo(){
         return this.saldo;
+    }
+
+    public String getTitular(){
+        return this.nome;
     }
 
 }
